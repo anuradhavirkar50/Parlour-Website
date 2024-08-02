62adbe7b5ae59aa913eb3d9208dc7d7e89c910cd
anuradhavirkar50 <146930154+anuradhavirkar50@users.noreply.github.com>
2024-08-02 18:24:24 +0530

    Add files via upload

---
+++ Parlour.js
@@ -0,0 +1,10 @@
+a = document.getElementById('content')
+console.log(a)
+e=document.getElementById('e')
+console.log(e) 
+
+
+function abc(){
+e.style.color="Yellow"
+e.style.backgroundColor="aqua"
+}
